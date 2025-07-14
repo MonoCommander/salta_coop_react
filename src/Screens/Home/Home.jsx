b3ebbe3a25db89ff42e2564c388a230650e31f2e
PGonzalezDev <pgonzalezdeveloper@gmail.com>
2025-07-14 20:23:02 -0300

    fixes in coop name & background image

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -5,8 +5,8 @@ const Home = () =>{
         <main className= "flex-shrink-0">
         <nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
                 <div className="container px-5">
-                    <img src="./public/512.png" alt="coop logo" width="50px"/>
-                    <a className="navbar-brand" ><span className="fw-bolder text-primary"> Cooperativa Salta</span></a>
+                    <img src="./public/COSEBYL.jpg" alt="coop logo" width="50px"/>
+                    <a className="navbar-brand" ><span className="fw-bolder text-primary"> Cooperativa Cosebyl</span></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
@@ -18,7 +18,7 @@ const Home = () =>{
                 </div>
             </nav>
             <section>   
-                <img src="./public/background.jpg" alt="background mountain" width="100%" />
+                <img src="./public/Quebrada_cafayate.jpeg" alt="background mountain" width="100%" />
             </section>
 
             <footer className="bg-white py-4 mt-auto">
--- src/Screens/Services/Services.jsx
+++ src/Screens/Services/Services.jsx
@@ -30,17 +30,6 @@ return(
                                     </div>
                                 </div>
                             </div>
-                            <div class="card overflow-hidden shadow rounded-4 border-0">
-                                <div class="card-body p-0">
-                                    <div class="d-flex align-items-center">
-                                        <div class="p-5">
-                                            <h2 class="fw-bolder">Lllama guerrillera</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius at enim eum illum aperiam placeat esse? Mollitia omnis minima saepe recusandae libero, iste ad asperiores! Explicabo commodi quo itaque! Ipsam!</p>
-                                        </div>
-                                        <img class="img-fluid" src="./public/combat-lama.jpg" width="300px" alt="..." />
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
